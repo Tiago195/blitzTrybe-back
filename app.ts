46fa7e8b0930f9659f81fbc39d3fc11c45438630
Tiago195 <tiagocf475@gmail.com>
2022-06-24 12:23:16 -0300

    rota get task implementada

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import user from './routes/users.routes';
+import task from './routes/tasks.routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/user', user);
+app.use('/task', task);
 
 export default app;
